656dbc1cb70f48f693772a3a9a6b6d1b7bcd7bea
Nhlalala <nhlalala.mudanisi@dadevs.co.za>
2023-02-20 16:28:23 +0200

    strapii

--- online-food-ordering-strapi/config/database.js
+++ online-food-ordering-strapi/config/database.js
@@ -1,11 +1,13 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', '1dpg-ceuhvhcgqg40d6i42hr0-a.oregon-postgres.render.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'food_ordering'),
+      user: env('DATABASE_USERNAME', 'admin'),
+      password: env('DATABASE_PASSWORD', 'pd4getjBi9IUIy3TGQLwNuscVF5I9lcl'),
+      ssl: env.bool('DATABASE_SSL', true),
     },
-    useNullAsDefault: true,
   },
 });
