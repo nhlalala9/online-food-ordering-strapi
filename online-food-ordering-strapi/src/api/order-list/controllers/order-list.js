bc16a0f3b55fd93c4bbe3cab9006d0a046a2eac3
ignatius <ignatius.mafahla@dadevs.co.za>
2023-02-28 09:22:36 +0200

    products

---
+++ online-food-ordering-strapi/src/api/order-list/controllers/order-list.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * order-list controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::order-list.order-list');
---
+++ online-food-ordering-strapi/src/api/order-list/routes/order-list.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * order-list router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::order-list.order-list');
